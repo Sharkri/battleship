c7f36286c550b22c3f4059b5958cbe88422af3ff
Sharkri <sharkdude955@gmail.com>
2022-10-12 21:13:55 -0500

    gameboard

---
+++ src/modules/Gameboard.js
@@ -0,0 +1,39 @@
+import Ship from "./Ship";
+
+export default class Gameboard {
+  constructor() {
+    this.board = [];
+    this.missedShots = [];
+    for (let i = 0; i < 7; i += 1) {
+      const row = [];
+      for (let j = 0; j < 7; j += 1) row.push(null);
+      this.board.push(row);
+    }
+  }
+
+  at(x, y) {
+    return this.board[y][x];
+  }
+
+  placeShip(length, x, y) {
+    if (this.board[y].slice(x).length < length) return;
+    const ship = Ship(length);
+    for (let i = 0; i < length; i += 1) this.board[y][x + i] = ship;
+  }
+
+  receiveAttack(x, y) {
+    const square = this.at(x, y);
+    if (square) square.hit();
+    else this.missedShots.push([x, y]);
+  }
+
+  isGameOver() {
+    for (let y = 0; y < 7; y += 1) {
+      for (let x = 0; x < 7; x += 1) {
+        const ship = this.at(x, y);
+        if (ship && !ship.isSunk()) return false;
+      }
+    }
+    return true;
+  }
+}
---
+++ src/tests/Gameboard.test.js
@@ -0,0 +1,53 @@
+import Gameboard from "../modules/Gameboard";
+
+it("should create a gameboard", () => {
+  const gameboard = new Gameboard();
+  // expect row and column to be 7x7
+  expect(gameboard.board.length).toBe(7);
+  expect(gameboard.board[0].length).toBe(7);
+});
+
+it("should place a ship", () => {
+  const gameboard = new Gameboard();
+  const length = 3;
+  const x = 0;
+  const y = 4;
+  gameboard.placeShip(length, x, y);
+  expect(gameboard.at(2, 4)).toBeTruthy();
+});
+
+it("should not allow invalid placement of a ship", () => {
+  const gameboard = new Gameboard();
+  const length = 6;
+  const x = 0;
+  const y = 0;
+  gameboard.placeShip(length, x, y);
+  expect(gameboard.at(6, 0)).toBeFalsy();
+});
+
+it("should receive attack correctly", () => {
+  const gameboard = new Gameboard();
+  const x = 2;
+  const y = 2;
+  gameboard.placeShip(2, x, y);
+  const ship = gameboard.at(x, y);
+  gameboard.receiveAttack(x, y);
+  gameboard.receiveAttack(x + 1, y);
+  expect(ship.isSunk()).toBeTruthy();
+});
+
+it("should record missed shots", () => {
+  const gameboard = new Gameboard();
+  gameboard.receiveAttack(5, 2);
+  expect(gameboard.missedShots[0]).toEqual([5, 2]);
+});
+
+it("should report if all ships sunk", () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(2, 0, 0);
+  gameboard.receiveAttack(0, 0);
+  gameboard.receiveAttack(1, 0);
+  expect(gameboard.isGameOver()).toBeTruthy();
+});
+
+it.todo("should be able to place a ship vertically");
