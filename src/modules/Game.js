9a49ed082e9fa3647a1b8916576c84dbaeccaeb4
Sharkri <sharkdude955@gmail.com>
2022-10-14 16:14:28 -0500

    changed player 2 to computer

--- src/modules/Game.js
+++ src/modules/Game.js
@@ -3,9 +3,9 @@ import Player from "./Player";
 export default class Game {
   constructor(name1, name2) {
     this.playerOne = Player(name1);
-    this.playerTwo = Player(name2);
+    this.playerTwo = Player(name2, "computer");
     // Hard coded locations for now
-    for (let i = 0; i < 4; i += 1) {
+    for (let i = 2; i < 6; i += 1) {
       this.playerOne.gameboard.placeShip(i, 0, i);
       this.playerTwo.gameboard.placeShip(i, 0, i);
     }
