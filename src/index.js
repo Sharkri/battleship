35ebe0455634d68ae3c6431b95b83e875ff07f02
Sharkri <sharkdude955@gmail.com>
2022-10-15 04:01:10 -0500

    add drag and drop functionality to place ships

--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ import Game from "./modules/Game";
 import displayController from "./displayController";
 
 const display = displayController();
+
 const restart = document.querySelector("#restart");
 const enemyBoard = document.querySelector(".enemy-board");
+const playerBoard = document.querySelector(".player-board");
+let draggedShip = null;
 let game = new Game();
 display.initialize(game.playerOne.gameboard, game.playerTwo.gameboard);
 
 restart.addEventListener("click", () => {
-  game = new Game();
+  const name1 = null;
+  const name2 = null;
+
+  game = new Game(name1, name2);
   display.initialize(game.playerOne.gameboard, game.playerTwo.gameboard);
 });
 
 enemyBoard.addEventListener("click", (e) => {
-  const { playerOne } = game;
-  const { playerTwo } = game;
-
+  const { playerOne, playerTwo } = game;
   // if already marked
   if (e.target.classList.length > 1) return;
 
@@ -45,3 +49,40 @@ enemyBoard.addEventListener("click", (e) => {
     // handle lose
   }
 });
+
+// Drag and drop ships
+playerBoard.addEventListener("drop", (e) => {
+  e.preventDefault();
+  const { playerOne } = game;
+  if (!e.target.classList.contains("square")) return;
+  e.target.classList.remove("drag-over");
+  const x = +e.target.getAttribute("data-coord-x");
+  const y = +e.target.getAttribute("data-coord-y");
+  const isVertical = draggedShip.dataset.vertical === "true";
+  playerOne.gameboard.placeShip(draggedShip.children.length, x, y, isVertical);
+
+  display.initialize(playerOne.gameboard);
+});
+
+playerBoard.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  const x = +e.target.getAttribute("data-coord-x");
+  const y = +e.target.getAttribute("data-coord-y");
+  const { length } = draggedShip.children;
+  const isVertical = draggedShip.dataset.vertical === "true";
+  if (game.playerOne.gameboard.isValidPosition(length, x, y, isVertical)) {
+    e.target.classList.add("drag-over");
+  } else e.target.classList.add("invalid");
+});
+
+playerBoard.addEventListener("dragleave", (e) => {
+  e.target.classList.remove("drag-over");
+  e.target.classList.remove("invalid");
+});
+
+const ships = document.querySelectorAll(".ships > div");
+ships.forEach((ship) =>
+  ship.addEventListener("dragstart", (e) => {
+    draggedShip = e.target;
+  })
+);
