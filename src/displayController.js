5d46639afcc6e49effcbe511b9a2cd008fa611a5
Sharkri <sharkdude955@gmail.com>
2022-10-16 01:33:29 -0500

    made displayController render things instead of index.js and other stuff

--- src/displayController.js
+++ src/displayController.js
@@ -1,22 +1,33 @@
 export default function displayController() {
   const boards = document.querySelectorAll(".board");
-  function initialize(playerBoard) {
-    boards.forEach((board) => {
-      // Clear the board
-      board.textContent = "";
-      for (let y = 0; y < 10; y += 1)
-        for (let x = 0; x < 10; x += 1) {
-          const square = document.createElement("button");
-          square.classList.add("square");
-          square.setAttribute("data-coord-x", x);
-          square.setAttribute("data-coord-y", y);
-          // mark square as a ship if on players board
-          if (board.classList.contains("player-board") && playerBoard.at(x, y))
-            square.classList.add("ship");
 
-          board.appendChild(square);
-        }
-    });
+  function renderBoard(board, boardIndex) {
+    // Clear the board
+    boards[boardIndex].textContent = "";
+    for (let y = 0; y < 10; y += 1)
+      for (let x = 0; x < 10; x += 1) {
+        const square = document.createElement("button");
+        square.classList.add("square");
+        square.setAttribute("data-coord-x", x);
+        square.setAttribute("data-coord-y", y);
+
+        if (board.isHit(x, y)) square.classList.add("hit");
+        if (board.isMiss(x, y)) square.classList.add("missed");
+
+        const ship = board.at(x, y);
+        // Only show the ship if it's on the players board
+        if (ship && boardIndex === 0) square.classList.add("ship");
+        if (ship?.isSunk()) square.classList.add("sunk");
+
+        boards[boardIndex].appendChild(square);
+      }
   }
-  return { initialize };
+
+  function render(...gameboards) {
+    for (let i = 0; i < gameboards.length; i += 1) {
+      renderBoard(gameboards[i], i);
+    }
+  }
+
+  return { render, renderBoard };
 }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ import Game from "./modules/Game";
 import displayController from "./displayController";
 
 const display = displayController();
-
+const ships = document.querySelectorAll(".ship");
 const startGame = document.querySelector("#start-game");
 const enemyBoard = document.querySelector(".enemy-board");
 const playerBoard = document.querySelector(".player-board");
-let draggedShip = null;
-let game = new Game();
-display.initialize(game.playerOne.gameboard, game.playerTwo.gameboard);
+let draggedShip;
+const game = new Game();
+display.render(game.playerOne.gameboard, game.playerTwo.gameboard);
 
 startGame.addEventListener("click", () => {
-  const name1 = null;
-  const name2 = null;
-
-  game = new Game(name1, name2);
-  display.initialize(game.playerOne.gameboard, game.playerTwo.gameboard);
+  startGame.disabled = true;
+  enemyBoard.classList.add("active");
 });
 
 enemyBoard.addEventListener("click", (e) => {
@@ -27,24 +24,19 @@ enemyBoard.addEventListener("click", (e) => {
   if (playerTwo.gameboard.isGameOver() || playerOne.gameboard.isGameOver())
     return;
 
-  const x = e.target.getAttribute("data-coord-x");
-  const y = e.target.getAttribute("data-coord-y");
+  const x = +e.target.getAttribute("data-coord-x");
+  const y = +e.target.getAttribute("data-coord-y");
 
-  const attack = playerOne.attack(playerTwo, x, y);
-  e.target.classList.add(attack.isHit ? "hit" : "missed");
-
+  playerOne.attack(playerTwo, x, y);
+  display.renderBoard(playerTwo.gameboard, 1);
   if (playerTwo.gameboard.isGameOver()) {
     // handle epic win
     return;
   }
 
   // make ai move after player move
-  const AIMove = playerTwo.makeRandomMove(playerOne);
-  const grid = document.querySelector(
-    `.player-board .square[data-coord-x="${AIMove.x}"][data-coord-y="${AIMove.y}"]`
-  );
-  grid.classList.add(AIMove.isHit ? "hit" : "missed");
-
+  playerTwo.makeRandomMove(playerOne);
+  display.renderBoard(playerOne.gameboard, 0);
   if (playerOne.gameboard.isGameOver()) {
     // handle lose
   }
@@ -53,15 +45,24 @@ enemyBoard.addEventListener("click", (e) => {
 // Drag and drop ships
 playerBoard.addEventListener("drop", (e) => {
   e.preventDefault();
-  const { playerOne } = game;
-  if (!e.target.classList.contains("square")) return;
+  const { playerOne, playerTwo } = game;
   e.target.classList.remove("drag-over");
+  e.target.classList.remove("invalid");
+
   const x = +e.target.getAttribute("data-coord-x");
   const y = +e.target.getAttribute("data-coord-y");
   const isVertical = draggedShip.dataset.vertical === "true";
-  playerOne.gameboard.placeShip(draggedShip.children.length, x, y, isVertical);
-  display.initialize(playerOne.gameboard);
+  const { length } = draggedShip.children;
+  if (!playerOne.gameboard.isValidPosition(length, x, y, isVertical)) return;
+
+  playerOne.gameboard.placeShip(length, x, y, isVertical);
+  display.render(playerOne.gameboard, playerTwo.gameboard);
   draggedShip.parentNode.classList.add("hidden");
+
+  // check if all ships are placed
+  if (!document.querySelector(".ship-container:not(.hidden)")) {
+    startGame.disabled = false;
+  }
 });
 
 playerBoard.addEventListener("dragover", (e) => {
@@ -70,27 +71,28 @@ playerBoard.addEventListener("dragover", (e) => {
   const y = +e.target.getAttribute("data-coord-y");
   const { length } = draggedShip.children;
   const isVertical = draggedShip.dataset.vertical === "true";
+
   if (game.playerOne.gameboard.isValidPosition(length, x, y, isVertical)) {
     e.target.classList.add("drag-over");
   } else e.target.classList.add("invalid");
 });
 
 playerBoard.addEventListener("dragleave", (e) => {
+  e.preventDefault();
   e.target.classList.remove("drag-over");
   e.target.classList.remove("invalid");
 });
 
-const ships = document.querySelectorAll(".ship");
 ships.forEach((ship) =>
   ship.addEventListener("dragstart", (e) => {
     draggedShip = e.target;
   })
 );
 const rotateBtns = document.querySelectorAll(".rotate");
-rotateBtns.forEach((rotateBtn) => {
+rotateBtns.forEach((rotateBtn) =>
   rotateBtn.addEventListener("click", (e) => {
     const ship = e.target.closest("svg").previousElementSibling;
     const isVertical = ship.getAttribute("data-vertical") === "true";
     ship.setAttribute("data-vertical", !isVertical);
-  });
-});
+  })
+);
