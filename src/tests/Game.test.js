c8b7a0e6885d9c7300bb490c153ae7f3f95ca30e
Sharkri <sharkdude955@gmail.com>
2022-10-17 00:30:57 -0500

    fix tests

--- src/tests/Game.test.js
+++ src/tests/Game.test.js
@@ -5,13 +5,3 @@ it("should set up a new game", () => {
   expect(game.playerOne).toBeTruthy();
   expect(game.playerTwo).toBeTruthy();
 });
-
-it("should let players place ships", () => {
-  const shipPlacements = [
-    { length: 5, x: 0, y: 0 },
-    { length: 3, x: 0, y: 1, isVertical: true },
-  ];
-  const game = new Game(null, null, shipPlacements);
-  expect(game.playerOne.gameboard.at(0, 0)).toBeTruthy();
-  expect(game.playerOne.gameboard.at(0, 2)).toBeTruthy();
-});
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -3,8 +3,10 @@ import Player from "../modules/Player";
 test("players should be able to attack eachother", () => {
   const player = Player("Joe");
   const player2 = Player("orangejuice");
-  player2.gameboard.placeShip(1, 3, 5);
+  const playerTwoBoard = player2.getBoard();
+  playerTwoBoard.placeShip(1, 3, 5);
   player.attack(player2, 3, 5);
-  expect(player2.gameboard.at(3, 5).isSunk()).toBeTruthy();
-  expect(player2.gameboard.isGameOver()).toBeTruthy();
+
+  expect(playerTwoBoard.at(3, 5).isSunk()).toBeTruthy();
+  expect(playerTwoBoard.isGameOver()).toBeTruthy();
 });
